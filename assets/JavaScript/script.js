ddba2fe08151162e9929759afa5eac6713e980f6
karan12011 <karanprakash12011abc@gmail.com>
2024-05-16 14:46:20 +0530

    Media Queries added

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,8 +1,22 @@
 const products = document.querySelectorAll('.product')
+const bar = document.getElementById("bar")
+const nav = document.getElementById("nav-items")
+const close = document.getElementById("close-nav")
 
 products.forEach(product => {
-    product.addEventListener("click",()=>{
-        console.log('hello')
-        window.location.href="/product.html";
-    })
+  product.addEventListener("click",()=>{
+    console.log('hello')
+    window.location.href="/product.html";
+  })
 });
+
+close.addEventListener('click',()=>{
+  nav.classList.remove("visible")
+  console.log("hello")
+})
+
+if (bar){
+  bar.addEventListener('click',()=> {
+    nav.classList.add('visible')
+  })
+}
